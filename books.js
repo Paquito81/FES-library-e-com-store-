578a8b786e38f58148a25579dd3940ee67540274
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-10-13 05:49:24 -0700

    add

--- books.js
+++ books.js
@@ -1,79 +1,42 @@
-let books;
 
-async function renderBooks(filter) {
-  const booksWrapper = document.querySelector(".books");
 
-  booksWrapper.classList += ' books__loading'
+function renderBooks() {
+  const booksWrapper = document.querySelector('.books');
 
-  if (!books) {
-    books = await getBooks();
-  }
+  const books = getBooks();
   
-  booksWrapper.classList.remove('books__loading')
-
-  if (filter === "LOW_TO_HIGH") {
-    books.sort(
-      (a, b) =>
-        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
-    );
-  } else if (filter === "HIGH_TO_LOW") {
-    books.sort(
-      (a, b) =>
-        (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)
-    );
-  } else if (filter === "RATING") {
-    books.sort((a, b) => b.rating - a.rating);
-  }
-
-  const booksHtml = books
-    .map((book) => {
-      return `<div class="book">
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
     <figure class="book__img--wrapper">
-      <img class="book__img" src="${book.url}" alt="">
+       <img src="${book.url}" alt="" class="book__img">
     </figure>
-    <div class="book__title">
-      ${book.title}
+    <div class="book__title">${book.title} 
     </div>
     <div class="book__ratings">
-      ${ratingsHTML(book.rating)}
+      <i class="fas fa-star" aria-hidden="true"></i>
+      <i class="fas fa-star" aria-hidden="true"></i>
+      <i class="fas fa-star" aria-hidden="true"></i>
+      <i class="fas fa-star" aria-hidden="true"></i>
+      <i class="fas fa-star-half-alt" aria-hidden="true"></i>
     </div>
     <div class="book__price">
-      ${priceHTML(book.originalPrice, book.salePrice)}
+      <span class="book__price--normal">${book.originalPrice}</span>
+        ${book.salePrice}
     </div>
   </div>`;
-    })
-    .join("");
+  });
+  .join("");
 
   booksWrapper.innerHTML = booksHtml;
 }
 
-function priceHTML(originalPrice, salePrice) {
-  if (!salePrice) {
-    return `$${originalPrice.toFixed(2)}`;
-  }
-  return `<span class="book__price--normal">$${originalPrice.toFixed(
-    2
-  )}</span>$${salePrice.toFixed(2)}`;
+function = filterBooks(event) {
+  console.log(event.target.value)
 }
-
-function ratingsHTML(rating) {
-  let ratingHTML = "";
-  for (let i = 0; i < Math.floor(rating); ++i) {
-    ratingHTML += '<i class="fas fa-star"></i>\n';
-  }
-  if (!Number.isInteger(rating)) {
-    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
-  }
-  return ratingHTML;
-}
-
-function filterBooks(event) {
-  renderBooks(event.target.value);
-}
-
 setTimeout(() => {
   renderBooks();
 });
+
 
 // FAKE DATA
 function getBooks() {
