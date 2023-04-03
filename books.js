df6c98f16ad4de57e9ef176f90b031bdb1a1a789
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2023-04-03 14:59:09 -0700

    books

--- books.js
+++ books.js
@@ -1,9 +1,22 @@
-function openMenu() {
-    document.body.classList += " menu--open"
-  }
-  
-  function closeMenu() {
-    document.body.classList.remove('menu--open')
-  }
-  
+function renderBooks() {
+    const booksWrapper = document.querySelector('.books');
 
+    booksWrapper.innerHTML =
+    `<div class="book">
+  <figure class="book__img--wrapper">
+    <img src="img/atomic habits.jpg" alt="" class="book__img" />
+  </figure>
+  <div class="book__title">Atomic Habits</div>
+  <div class="book__ratings">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star-half-alt"></i>
+  </div>
+  <div class="book__price">
+    <span class="book__price--normal">$59.95</span>
+    $14.95
+  </div>
+  </div>`
+}
